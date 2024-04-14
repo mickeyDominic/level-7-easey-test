5a44d55df5cdaaacf3112131b6ff95234398d6d6
Dominic <developer@osit.co.za>
2024-04-14 12:51:37 +0200

    AppUser Model

---
+++ app/Models/AppUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class AppUser extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    protected $table = 'user';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'date_of_birth',
+        'province',
+        'gender',
+        'admin',
+    ];
+}
