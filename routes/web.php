f061aa7cf7820073b9a863b432d45f54e263acf8
Dominic <developer@osit.co.za>
2024-04-14 12:50:41 +0200

    Routes for adding user(s)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AppController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,5 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/add-user', [AppController::class, 'viewAddPage'])
+    ->name('add-user');
+Route::post('/add-user', [AppController::class, 'addRecord'])
+    ->name('add-user');
 
 require __DIR__.'/auth.php';
