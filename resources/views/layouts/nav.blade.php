f1487b05e2b04a05dd28470604bee230a3befc15
Dominic <developer@osit.co.za>
2024-04-14 17:49:49 +0200

    Refactored

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,7 +2,7 @@
     <div class="navbar-header">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="/">Home</a>
+                <a class="navbar-brand" href="/list-users">Home</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
